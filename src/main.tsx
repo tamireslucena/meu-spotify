202afb0bd1a1691870ded8c2e475969a93bccdfb
Tamires Lucena <tamiresb.lucena@gmail.com>
2024-04-15 00:57:13 -0300

    refactor: reestructure routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,24 +13,47 @@ import Home from "./pages/Home";
 import Artists from "./pages/Artists";
 import Playlists from "./pages/Playlists";
 import Discography from "./pages/Artists/Discography";
+import Template from "./components/Template";
+import Callback from "./pages/Callback";
+import AuthCheck from "./components/AuthCheck";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Login />}></Route>
-        <Route path="home" element={<Home />}></Route>
-        <Route path="artists" element={<Artists />}></Route>
-        <Route path="playlists" element={<Playlists />}></Route>
-        <Route path="profile" element={<Profile />}></Route>
-        <Route path="artists/:id/albums" element={<Discography />}></Route>
+        <Route
+          path="/"
+          element={
+            <AuthCheck>
+              <Template />
+            </AuthCheck>
+          }
+        >
+          <Route index path="" element={<Home />}></Route>
+          <Route path="artists" element={<Artists />}></Route>
+          <Route path="playlists" element={<Playlists />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+          <Route path="artists/:id/albums" element={<Discography />}></Route>
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/callback" element={<Callback />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
